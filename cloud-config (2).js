c8222e0c1121ad4021d975e1ea55b5aaa62f7967
davidgideonkatimbo-del <davidgideonkatimbo@gmail.com>
2026-09-26 11:34:14 +0300

    Refactor cloud config with new payment links
    Updated supabaseUrl and supabaseAnonKey to use double quotes. Added stripePaymentLink and paypalLink.

--- cloud-config (2).js
+++ cloud-config (2).js
@@ -1,10 +1,7 @@
 window.TAPEHEAD_CLOUD = {
   enabled: true,
-  supabaseUrl: 'https://dswhlqropbraofcrsdcj.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzd2hscXJvcGJyYW9mY3JzZGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyNjk1NjcsImV4cCI6MjEwNTg0NTU2N30.Le4_PkJw0BkCQndBc2fFblcxl_iiEcVdtoYVAwXPuiI',
-  currency: 'USD',
-  provider: 'flutterwave',
-  payApiUrl: '',
-  stripePaymentLink: '',
-  paypalLink: ''
+  supabaseUrl: "https://dswhlqropbraofcrsdcj.supabase.co",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzd2hscXJvcGJyYW9mY3JzZGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyNjk1NjcsImV4cCI6MjEwNTg0NTU2N30.Le4_PkJw0BkCQndBc2fFblcxl_iiEcVdtoYVAwXPuiI",
 };
+window.TAPEHEAD_CLOUD.stripePaymentLink = 'https://buy.stripe.com/your_link';
+window.TAPEHEAD_CLOUD.paypalLink = 'https://paypal.me/yourname';
