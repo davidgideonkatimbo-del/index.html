bcecdb137414a09540c27c3a7808121a51820878
davidgideonkatimbo-del <davidgideonkatimbo@gmail.com>
2026-09-26 11:14:53 +0300

    Delete cloud-config.js

--- cloud-config.js
+++
@@ -1,8 +0,0 @@
-window.TAPEHEAD_CLOUD = {
-  enabled: true,
-  supabaseUrl: "https://dswhlqropbraofcrsdcj.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzd2hscXJvcGJyYW9mY3JzZGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyNjk1NjcsImV4cCI6MjEwNTg0NTU2N30.Le4_PkJw0BkCQndBc2fFblcxl_iiEcVdtoYVAwXPuiI"
-};
-window.TAPEHEAD_CLOUD = window.TAPEHEAD_CLOUD || {};
-window.TAPEHEAD_CLOUD.stripePaymentLink = window.TAPEHEAD_CLOUD.stripePaymentLink || '';
-window.TAPEHEAD_CLOUD.paypalLink = window.TAPEHEAD_CLOUD.paypalLink || '';
